a814ae50a8a2b3a09de31e6ed96a99a563b143f4
senturner <stturner@student.unimelb.edu.au>
2021-05-06 02:49:55 +0000

    wrangled depression data

---
+++ python_scripts/depression.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_excel('../wrangled/depression.xlsx', engine = 'openpyxl')
+
+df = df.drop([0,1])
+df = df[['Unnamed: 0','Persons']]
+df = df.rename(columns={'Unnamed: 0': 'LGA', 'Persons': 'Depression Rate'})
+
+df['LGA'] = df['LGA'].replace('\([a-zA-Z]*\)','', regex = True)
+
+df = df.set_index('LGA')
+
+df.to_csv('../wrangled/depression.csv')
