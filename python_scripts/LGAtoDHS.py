488bad84340e9e3209fe0d5c2bdc10149959b8a7
senturner <stturner@student.unimelb.edu.au>
2021-05-08 07:25:09 +0000

    added new data points and added DHS Area to depression.csv

--- python_scripts/LGAtoDHS.py
+++ python_scripts/LGAtoDHS.py
@@ -3,12 +3,12 @@
 import pandas as pd
 from collections import defaultdict
 
-df = pd.read_excel('./raw_data/LGA-DHS.xlsx', engine = 'openpyxl')
+df = pd.read_excel('../raw_data/LGA-DHS.xlsx', engine = 'openpyxl')
 df = df[['LGA Name', 'Departmental Area']]
 
 # Preprocess LGA names
 
-df['LGA Name'] = df['LGA Name'].replace('\([a-zA-Z]*\)','', regex = True)
+df['LGA Name'] = df['LGA Name'].replace('\([a-zA-Z]*\)','', regex = True).str.strip()
 
 LGAtoDHS = {}
 DHStoLGA = defaultdict(list)
@@ -16,3 +16,10 @@ DHStoLGA = defaultdict(list)
 for i in range(len(df)):
     LGAtoDHS[df.iloc[i, 0]] = df.iloc[i,1]
     DHStoLGA[df.iloc[i, 1]].append(df.iloc[i,0])
+    
+df = pd.read_csv('../wrangled/depression.csv')
+
+df['DHS_Area'] = df.LGA.map(LGAtoDHS)
+df = df.set_index('LGA')
+
+df.to_csv('../wrangled/depression2.csv')
