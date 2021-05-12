2f86dbd1de18c34b3a5bee5a7908d42cd6298c75
Steven Nguyen <steven.n.1618@gmail.com>
2021-05-12 16:32:08 +1000

    added absecnes

--- merger.py
+++ merger.py
@@ -24,8 +24,9 @@ DHS_data.to_csv('./wrangled/DHS_data.csv')
 # DHS processing
 tsratio = pd.read_csv('./wrangled/tsRatio.csv', index_col = 0)
 depression = pd.read_csv('./wrangled/depression.csv', index_col = 'LGA')
+absences = pd.read_csv('./wrangled/absences.csv', index_col = 'LGA')
 aedc = pd.read_csv('./wrangled/AEDC.csv', index_col = 'LGA')
 LGAfiles = {k: int(v) for line in open('./raw_data/vcamsLGA/names.txt', "r") for (k,v) in [line.split()]}
 vcams_LGA_data = [vcams.LGA(i, year = LGAfiles[i]) for i in LGAfiles.keys()]
-LGA_data = depression.join(vcams_LGA_data).join(aedc).join(tsratio)
+LGA_data = depression.join(vcams_LGA_data).join(aedc).join(tsratio).join(absences)
 LGA_data.to_csv('./wrangled/LGA_data.csv')
