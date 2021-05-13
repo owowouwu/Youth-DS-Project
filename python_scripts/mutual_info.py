9ec5a3642945b8bf6a421eb0b9561cf6f779c83b
Steven Nguyen <steven.n.1618@gmail.com>
2021-05-13 12:35:38 +1000

    calculate MI

--- python_scripts/mutual_info.py
+++ python_scripts/mutual_info.py
@@ -1,13 +1,10 @@
 import pandas as pd
 from sklearn.feature_selection import mutual_info_regression
 
-df = pd.read_csv('../wrangled/fullTable.csv')
 
-wanted_cols = ['LGA', 'Depression Rate', 'Social (vulnerable)', 'Language (vulnerable)', 'Communication (Vulnerable)', 'Emotional (Vulnerable)', 'Health (Vulnerable)', 'tsRatio', 'bullying', 'connectedness', 'familystress']
-
-df = df[wanted_cols].set_index('LGA')
-df = df.drop(['Queenscliffe', 'Mansfield', 'Pyrenees', 'West Wimmera'])
-
+# LGA areas
+df = pd.read_csv('../wrangled/LGA_data.csv', index_col = 'LGA')
+df = df.dropna()
 indep_var = ['Depression Rate']
 dep_vars = df.columns.difference(indep_var).to_list()
 
@@ -17,3 +14,4 @@ dep_vars = df.columns.difference(indep_var).to_list()
 
 for var in dep_vars:
     print((var, float(mutual_info_regression(df[indep_var], df[var]))))
+    
