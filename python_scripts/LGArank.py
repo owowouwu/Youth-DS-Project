8da031a7421875941b16271bf8dee391ff91a4e3
LinaXZhu <80029963+LinaXZhu@users.noreply.github.com>
2021-05-16 00:59:14 +1000

    Update LGArank.py

--- python_scripts/LGArank.py
+++ python_scripts/LGArank.py
@@ -5,4 +5,9 @@ df = pd.read_csv("../wrangled/LGA_data.csv.csv")
 ascCols=['connectedness', 'yr12completion']
 df[ascCols].rank()
 df.columns.difference(ascCols).rank()
+# normalise
+max(df[ascCols].rank())-df[ascCols].rank()
+max(df.columns.difference(ascCols).rank())-df.columns.difference(ascCols).rank()
+# ranksum
+df['RankSum'] = df.sum(axis=1)
 df.to_csv('../wrangled/LGArank.csv')
