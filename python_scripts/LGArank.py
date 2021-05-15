5b27f7cf3952d8281114952f9b5c6ffdd422e75b
Steven Nguyen <steven.n.1618@gmail.com>
2021-05-16 01:22:44 +1000

    adjusted ranksum code

--- python_scripts/LGArank.py
+++ python_scripts/LGArank.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv("../wrangled/LGA_data.csv.csv")
+df = pd.read_csv("../wrangled/LGA_data.csv")
+rankDf = df.copy()
 
 ascCols=['connectedness', 'yr12completion'] # higher the better cols
-df[ascCols].rank()
-df.columns.difference(ascCols).rank()
+rankDf[ascCols] = df[ascCols].rank()
+rankDf[df.columns.difference(ascCols)] = df[df.columns.difference(ascCols)].rank()
 # normalise
-max(df[ascCols].rank())-df[ascCols].rank()
-max(df.columns.difference(ascCols).rank())-df.columns.difference(ascCols).rank()
+#rankDf[ascCols] = max(rankDf[ascCols])-rankDf[ascCols]
+#rankDf[ascCols] = max(rankDf[df.columns.difference(ascCols)])-rankDf[df.columns.difference(ascCols)]
 # ranksum
-df['RankSum'] = df.sum(axis=1)
-df.to_csv('../wrangled/LGArank.csv')
+#rankDf['RankSum'] = rankDf.sum(axis=1)
+#rankDf.to_csv('../wrangled/LGArank.csv')
